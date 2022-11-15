73faf1f92245019c6cde0eb26f122c39ffa84096
Durga prasad <Durga prasad@LAPTOP-LN9V9DGH>
2022-11-15 13:59:05 +0530

    first

---
+++ src/main/java/MySonar123/MyTest123/App.java
@@ -0,0 +1,13 @@
+package MySonar123.MyTest123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MySonar123/MyTest123/AppTest.java
@@ -0,0 +1,20 @@
+package MySonar123.MyTest123;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
